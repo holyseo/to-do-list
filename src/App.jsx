5b0ed29a74caa07f55610601cb384bc9fc08d9d6
holyseo <128103221+holyseo@users.noreply.github.com>
2024-06-24 14:19:39 +0100

    save changes from previous commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Todo from "./components/Todo";
 
 const App = () => {
   return (
-    <div className="p-10 mt-10 bg-stone-900 h-svh">
+    <div className="p-10 mt-10 bg-stone-900 h-dvh">
       <Todo />
     </div>
   );
